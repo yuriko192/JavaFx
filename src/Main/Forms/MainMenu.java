62fc02324f42818e24aaeb09bbbbb4502d4b251e
yuriko192 <ivanwijaya19@yahoo.co.id>
2019-09-11 21:01:26 +0700

    added button and global functions

--- src/Main/Forms/MainMenu.java
+++ src/Main/Forms/MainMenu.java
@@ -2,48 +2,29 @@ package Main.Forms;
 
 import Main.Main;
 import Main.Resources.GlobalVar;
+import Main.Utils.ButtonFunction;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 
-public class MainMenu extends FlowPane implements EventHandler<ActionEvent> {
-    private Button Rooms, Reservation, Services,FloorPlan;
+public class MainMenu extends FlowPane {
+    private Button Rooms_btn, Reservation_btn, Services_btn,FloorPlan_btn;
 
     public MainMenu() {
         super();
         //==========================
-        Rooms = new Button("Rooms");
-        Rooms.setOnAction(this);
-        GlobalVar.formatMainButton(Rooms);
+        Rooms_btn = new ButtonFunction("Rooms",GlobalVar.fun_Rooms,1);
+        Reservation_btn = new ButtonFunction("Reservation",GlobalVar.fun_MainMenu,1);
+        Services_btn = new ButtonFunction("Services",GlobalVar.fun_MainMenu,1);
+        FloorPlan_btn = new ButtonFunction("Floor Plan",GlobalVar.fun_FloorPlan,1);
 
-        Reservation = new Button("Reservation");
-        Reservation.setOnAction(this);
-        GlobalVar.formatMainButton(Reservation);
-
-        Services = new Button("Services");
-        Services.setOnAction(this);
-        GlobalVar.formatMainButton(Services);
-
-        FloorPlan = new Button("FloorPlan");
-        FloorPlan.setOnAction(this);
-        GlobalVar.formatMainButton(FloorPlan);
         //==================================
         this.setHgap(70);
         this.setVgap(25);
         this.setAlignment(Pos.CENTER);
-        this.getChildren().addAll(Rooms,Reservation,Services,FloorPlan);
+        this.getChildren().addAll(Rooms_btn,Reservation_btn,Services_btn,FloorPlan_btn);
     }
 
-    @Override
-    public void handle(ActionEvent actionEvent) {
-        if (actionEvent.getSource() == Rooms) {
-            Main.mainstage.setScene(GlobalVar.temp);
-        } else if (actionEvent.getSource() == FloorPlan) {
-            Main.mainstage.setScene(GlobalVar.floorplan_S);
-        } else {
-
-        }
-    }
 }
