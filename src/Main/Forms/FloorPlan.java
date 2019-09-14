cc213bc99513904e21e8a4a6dff5dade0486d83a
yuriko192 <ivanwijaya19@yahoo.co.id>
2019-09-14 10:34:14 +0700

    Added floorpane Content

--- src/Main/Forms/FloorPlan.java
+++ src/Main/Forms/FloorPlan.java
@@ -4,33 +4,55 @@ import Main.Resources.GlobalVar;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 
-public class FloorPlan extends WindowBase{
+
+public class FloorPlan extends WindowBase {
+    private final int FLOORS = 5;
 
     public FloorPlan() {
         super();
-
+        //=========List Lantai====================
+        //-----initialize scrollpanel-------
         ScrollPane floorList = new ScrollPane();
-        floorList.setMinWidth(130);
-        floorList.setPrefHeight(GlobalVar.HEIGHT*0.75);
-
-        Button[] floorarr = new Button[5];
-        for (int i=0;i<5;i++){
-            floorarr[i] = new Button("Floor 0"+ Integer.toString(i+2));
-            floorarr[i].setPrefWidth(120);
+        floorList.setPrefWidth(130);
+        floorList.setPrefHeight(GlobalVar.HEIGHT * 0.75);
+        //------initialize button---------
+        Button[] floorarr = new Button[FLOORS];
+        for (int i = 0; i < FLOORS; i++) {
+            floorarr[i] = new Button("Floor 0" + Integer.toString(i + 2));
+            floorarr[i].setPrefWidth(128);
             floorarr[i].setPrefHeight(40);
 
         }
-
+        //------initialize vbox--------
         VBox floors = new VBox();
         floors.setAlignment(Pos.CENTER);
+        //------gabungin-------------
         floors.getChildren().addAll(floorarr);
         floorList.setContent(floors);
+        this.add(floorList, 1, 1);
+        //==========================================
+        TextField searchBar = new TextField();
+        searchBar.setPrefWidth(130);
+        this.add(searchBar, 1, 2);
+        Button searchButton = new Button("Search");
 
-        this.add(floorList,1,1);
+        HBox searchArea = new HBox(10);
+        searchArea.setAlignment(Pos.BASELINE_RIGHT);
+        searchArea.getChildren().add(searchButton);
+        this.add(searchArea, 1, 3);
 
+
+        ScrollPane listkamarContainer = new ScrollPane();
+        listkamarContainer.setPrefWidth(550);
+        FlowPane listKamar = new FlowPane();
+        //size isinya 273 * 100
+
+        listkamarContainer.setContent(listKamar);
+        this.add(listkamarContainer,2,1,3,4);
     }
 }
